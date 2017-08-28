056665e8a7d7973464719d9a5584ac62bd956ea4
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-28 17:11:16 -0500

    main view and current view are working

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ let $ = require('jquery');
 let DOM = require('./dom_builder');
 let apiCalls = require('./api_calls');
 
+
+
 //load zip code form on page load
 $(window).ready(function(){
-	// DOM.loadZipForm();
-	apiCalls.getWeatherData('weather', '37206').then((data) =>{
-		DOM.loadCurrent(data);
-	});
+	DOM.loadZipForm();
 });
