20d5756bc7c857986d2d7af18e4c08385faee2ab
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-30 13:03:27 -0500

    built all DOM loader functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 'use strict';
 let $ = require('jquery');
 let apiCalls = require('./api_calls');
-let DOM = require('./dom_builder');
+let DomBuilder = require('./dom_builder');
+let DOM = DomBuilder.DOM;
 
 
-//load zip code form on page load
-// $(window).ready(function(){
-// 	DOM.loadZipForm();
-// });
+// load zip code form on page load
+$(window).ready(function(){
+	DOM.loadZipForm();
+});
 
 //testing
-$(window).ready(()=>{
-	apiCalls.getWeatherData('forecast', 37206)
-	.then((data)=>{
-		console.log("data", data);
-		DOM.DOM.load3Day(data);
-	});
-});
+// $(window).ready(()=>{
+// 	apiCalls.getWeatherData('forecast', 37206)
+// 	.then((data)=>{
+// 		console.log("data", data);
+// 		DOM.DOM.load3Day(data);
+// 	});
+// });
+
+// //testing
+// $(window).ready(()=>{
+// 	apiCalls.getWeatherData()
+// 	.then((data)=>{
+// 		DOM.loadCurrent(data);
+// 	});
+// });
